95497cccb12c512a0e8739c8de75ddcfb8929510
Hanna <marmalademarmoset@gmail.com>
2022-11-07 13:20:11 -0800

    commit tree de4ea081ebf16b93dbfede092482050051e28f13

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let Numfeet = f
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(f){
+  if (f <= 199) {
+    return 'This one is on me!'
+
+} else if (f <= 1500) {
+    return 'That will be twenty bucks.'
+
+} else if (f <= 2001) {
+  return 'I will gladly take your thirty bucks.'
+
+} else if (f>2500) {
+scuberGreetingForFeet = false
+  return 'No can do.'
+}};
+
+const City = ''
+
+function ternaryCheckCity(City){
+  ternaryCheckCity = City ==='NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
 function switchOnCharmFromTip(){
